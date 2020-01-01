b860525d100ff0070d80e5649b40f8c29f7440b7
vijay <universal.test.web@gmail.com>
2020-01-01 14:52:51 +0530

    Initial import

---
+++ Owl/Controller/Index/Index.php
@@ -0,0 +1,23 @@
+<?php
+	namespace Slide\Owl\Controller\index;
+	use Magento\Framework\App\Action\Context;
+	class Index extends \Magento\Framework\App\Action\Action
+	{
+		protected $_pageFactory;
+		public function __construct
+		(
+			\Magento\Framework\App\Action\Context $context,
+		\Magento\Framework\View\Result\PageFactory $pageFactory
+		)
+		{
+			$this->_pageFactory = $pageFactory;
+			return parent::__construct($context);
+		}
+		public function execute()
+		{
+			//die('hi');
+			return $this->_pageFactory->create();
+		}
+	}
+
+?>
